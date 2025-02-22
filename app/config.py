be74bc44f0046c6fa0892f55a70e9f016050282c
£Marve <45086740+VirusEmp@users.noreply.github.com>
2025-02-22 08:57:15 -0400

    feat(KEYS):Telex and API

--- app/config.py
+++ app/config.py
@@ -5,9 +5,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///complaints.db")
     
     # Telex integration settings
-    TELEX_TOKEN = os.environ.get("TELEX_TOKEN", "telex_secret_token")
+    TELEX_TOKEN = os.environ.get("TELEX_TOKEN", "9cy2u2VF5pVxYKxcHOl8")
     TELEX_TARGET_URL = os.environ.get("TELEX_TARGET_URL", "https://ping.telex.im/v1/webhooks/01951578-b933-7f1a-a6db-9984ac50486f")
     TELEX_API_KEY = os.environ.get("TELEX_API_KEY", "9cy2u2VF5pVxYKxcHOl8")
     
     # Application base URL
-    APP_BASE_URL = os.environ.get("APP_BASE_URL", "https://customer-feedback-api-ckii.onrender.com/all")
+    APP_BASE_URL = os.environ.get("APP_BASE_URL", "https://customer-feedback-api-ckii.onrender.com/all?token=9cy2u2VF5pVxYKxcHOl8")
