ca44a8d381ffb9d71c310c75960dc49682bb2d9d
£Marve <45086740+VirusEmp@users.noreply.github.com>
2025-02-24 09:32:11 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Get absolute path
     templates_path = os.path.join(BASE_DIR, "/Customer-Feedback-api/templates")   # Correct path
 
-    app = Flask(__name__, template_folder=/Customer-Feedback-api/templates)
+    app = Flask(__name__, template_folder=templates_path)
     CORS(app)
     app.config.from_object(Config)
 
